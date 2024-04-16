70a1fdabedcfd865b372ef1ec4966d781e343310
leandro <leandro.rodrigues00@hotmail.com>
2024-04-16 12:55:41 -0300

    feat: add drizzle and neon settings

---
+++ database/drizzle.ts
@@ -0,0 +1,11 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import { env } from "@/env";
+
+import * as schema from "./schema";
+
+const sql = neon(env.DATABASE_URL);
+const db = drizzle(sql, { schema });
+
+export default db;
--- src/env.ts
+++ src/env.ts
@@ -4,14 +4,14 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     CLERK_SECRET_KEY: z.string().min(1),
+    DATABASE_URL: z.string().url(),
   },
 
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
 
-  runtimeEnv: {
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  experimental__runtimeEnv: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
