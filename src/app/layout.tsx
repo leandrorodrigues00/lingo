efff1201fa017d412d3014a8ab8173d59f3c9493
leandro <leandro.rodrigues00@hotmail.com>
2024-04-23 21:24:52 -0300

    feat: add exit modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 import { Toaster } from "@/components/ui/sonner";
+import { ExitModal } from "@/components/exit-modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={font.className}>
           <Toaster />
+          <ExitModal />
           {children}
         </body>
       </html>
