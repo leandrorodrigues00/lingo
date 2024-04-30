3bacf2c9df4182a0aea164afc37b5a0fe7023910
leandro <leandro.rodrigues00@hotmail.com>
2024-04-30 20:42:33 -0300

    feat: add quests page

---
+++ src/app/(main)/quests/page.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+import { getUserProgress, getUserSubscription } from "@/database/queries";
+import { quests } from "@/config/quests";
+import { Progress } from "@/components/ui/progress";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
+export default async function QuestsPage() {
+  const userProgressData = getUserProgress();
+  const userSubscriptionData = getUserSubscription();
+
+  const [userProgress, userSubscription] = await Promise.all([
+    userProgressData,
+    userSubscriptionData,
+  ]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  const isPro = !!userSubscription?.isActive;
+
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={isPro}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <div className="flex w-full flex-col items-center">
+          <Image src="/icons/quests.svg" alt="quests" height={90} width={90} />
+          <h1 className="my-6 text-center text-2xl font-bold text-neutral-800">
+            Quests
+          </h1>
+          <p className="mb-6 text-center text-lg text-muted-foreground">
+            Complete quests by earning points.
+          </p>
+          <ul className="w-full">
+            {quests.map((quest) => {
+              const progress = (userProgress.points / quest.value) * 100;
+
+              return (
+                <div
+                  className="flex w-full items-center gap-x-4 border-t-2 p-4"
+                  key={quest.title}
+                >
+                  <Image
+                    src="/icons/points.svg"
+                    alt="Points"
+                    width={60}
+                    height={60}
+                  />
+                  <div className="flex w-full flex-col gap-y-2">
+                    <p className="text-xl font-bold text-neutral-700">
+                      {quest.title}
+                    </p>
+                    <Progress className="h-3" value={progress} />
+                  </div>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </FeedWrapper>
+    </div>
+  );
+}
